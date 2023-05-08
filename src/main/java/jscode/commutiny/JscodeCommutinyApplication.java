7988b4a32556ba116890c68b3bf8a783add2781b
eungyu-cho <eungyu.cho@toss.im>
2023-05-08 22:08:22 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/jscode/commutiny/JscodeCommutinyApplication.java
@@ -0,0 +1,13 @@
+package jscode.commutiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JscodeCommutinyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JscodeCommutinyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jscode/commutiny/JscodeCommutinyApplicationTests.java
@@ -0,0 +1,13 @@
+package jscode.commutiny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JscodeCommutinyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
